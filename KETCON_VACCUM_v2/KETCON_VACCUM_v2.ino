bc2f6c16113727982e87045ae19fa6fa40f56568
ashwinkjoseph <ashwinkjoseph@gmail.com>
2017-02-05 22:12:21 +0530

    updates

--- KETCON_VACCUM_v2/KETCON_VACCUM_v2.ino
+++ KETCON_VACCUM_v2/KETCON_VACCUM_v2.ino
@@ -9,7 +9,6 @@ int i;
 int dat0;
 int dat1;
 int dat2;
-int dat3;
 
 void setup() {
   // Motor Pins
@@ -21,7 +20,6 @@ void setup() {
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
 }
 
 void loop() {
@@ -29,17 +27,10 @@ void loop() {
   dat0 = analogRead(A0);
   dat1 = analogRead(A1);
   dat2 = analogRead(A2);
-  dat3 = analogRead(A3);
   //Checks if the vaccum has reached a dead end ahead
-  if(dat0 < 300){
+  if(dat0 > 400){
+    Serial.println("I'm In");
     // case when there is not enough space left behind to move back and there is not enough space ahead
-    if((dat3 < 200)&&(dat0 < 200)){
-      analogWrite(leftmotor_a, 0);
-      analogWrite(leftmotor_b, 0);
-      analogWrite(rightmotor_a, 255);
-      analogWrite(rightmotor_b, 0);
-    }
-    else{
       // give signals to make the motors move back
       analogWrite(leftmotor_a, 0);
       analogWrite(leftmotor_b, 255);
@@ -47,43 +38,45 @@ void loop() {
       analogWrite(rightmotor_b, 255);
       i = 0;
       // keeps the vaccum moving back for 5 seconds unless an obstacle is encountered
-      while((dat3 > 200)&&(i<100)){
-        i++;
-        delay(50);
-      }
+      delay(1250);
       // checks to see if there is any space to move to in the direction of dat2 and if there is, moves in that direction
-      if(dat2 > 200){
-        analogWrite(leftmotor_a, 125);
+      if(dat2 < 200){
+        Serial.println("Im' turning to dat2");
+        analogWrite(leftmotor_a, 255);
         analogWrite(leftmotor_b, 0);
-        analogWrite(rightmotor_a, 255);
-        analogWrite(rightmotor_b, 0);
+        analogWrite(rightmotor_a, 0);
+        analogWrite(rightmotor_b, 255);
+        delay(500);
       }
       else{
         // same as previous but to opposite direction
-        if(dat1 > 200){
-          analogWrite(leftmotor_a, 255);
-          analogWrite(leftmotor_b, 0);
-          analogWrite(rightmotor_a, 125);
+        if(dat1 < 200){
+          Serial.println("I'm turning to dat1");   
+          analogWrite(leftmotor_a, 0);
+          analogWrite(leftmotor_b, 255);
+          analogWrite(rightmotor_a, 255);
           analogWrite(rightmotor_b, 0);
+          delay(500);
         }
       }
-    }
   }
   else{
     // if there is an obstacle in this side, it moves in the opposite side
-    if(dat1 < 200){
-      analogWrite(leftmotor_a, 125);
-      analogWrite(leftmotor_b, 0);
+    if(dat1 > 300){
+      analogWrite(leftmotor_a, 0);
+      analogWrite(leftmotor_b, 255);
       analogWrite(rightmotor_a, 255);
       analogWrite(rightmotor_b, 0);
+      delay(500);
     }
     else{
       // if there is an obstacle in this direction, it moves in the opposite side
-      if(dat2 < 200){
+      if(dat2 > 300){
         analogWrite(leftmotor_a, 255);
         analogWrite(leftmotor_b, 0);
-        analogWrite(rightmotor_a, 125);
-        analogWrite(rightmotor_b, 0);
+        analogWrite(rightmotor_a, 0);
+        analogWrite(rightmotor_b, 255);
+        delay(500);
       }
       // if there is no obstacle in any direction, it keeps moving forward
       else{
